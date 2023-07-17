58d1834808fa797d43418c65ec1c28baafd82a8b
Kelechukwu <paparazzip8@gmail.com>
2023-07-17 05:08:36 +0100

    changing svgs to icons

--- src/app/components/SideBarNav.jsx
+++ src/app/components/SideBarNav.jsx
@@ -1,28 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import { BsTools } from "react-icons/bs";
+import { BiLogOut } from "react-icons/bi";
+import { TbMessage2Pin } from "react-icons/tb";
 
 const SideBarNav = () => {
 	return (
 		<section>
-			<div className="bg-slate-900 h-screen  pt-8 px-4 pb-4 justify-between flex-col  w-80">
+			<div className="bg-slate-900 h-screen pt-8 md:px-4 pb-4 justify-between flex-col  md:w-80">
 				<div className=" text-white md:w-32 h-[40rem]">
 					<Link href="/jobs">
 						<div className="hover:bg-blue-900 w-full p-2 rounded md:px-4 flex transition-all duration-300">
 							<div className="px-2">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth="1.5"
-									stroke="currentColor"
-									className="w-6 h-6"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-									/>
-								</svg>
+								<BsTools />
 							</div>
 							<div className="hidden md:block">Jobs</div>
 						</div>
@@ -30,20 +20,7 @@ const SideBarNav = () => {
 					<Link href="/account">
 						<div className="hover:bg-blue-900 w-full p-2 rounded md:px-4 flex transition-all duration-300">
 							<div className="px-2">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth="1.5"
-									stroke="currentColor"
-									className="w-6 h-6"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-									/>
-								</svg>
+								<TbMessage2Pin />
 							</div>
 							<div className="hidden md:block">Account</div>
 						</div>
@@ -115,20 +92,7 @@ const SideBarNav = () => {
 					<Link href={"/"}>
 						<div className="hover:bg-blue-900 w-full p-2 rounded md:px-4 flex transition-all duration-300">
 							<div className="px-2">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth="1.5"
-									stroke="currentColor"
-									className="w-6 h-6"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-									/>
-								</svg>
+								<BiLogOut />
 							</div>
 							<div className="hidden md:block">Logout</div>
 						</div>
--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
+import { IoMenuOutline } from "react-icons/bi";
+import { IoCloseOutline } from "react-icons/bi";
 
 const Navbar = () => {
 	const [open, setOpen] = useState(false);
@@ -28,37 +30,7 @@ const Navbar = () => {
 								setOpen(!open);
 							}}
 						>
-							{!open ? (
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth="1.5"
-									stroke="currentColor"
-									className="w-6 h-6"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-									/>
-								</svg>
-							) : (
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth="1.5"
-									stroke="currentColor"
-									className="w-6 h-6"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M6 18L18 6M6 6l12 12"
-									/>
-								</svg>
-							)}
+							{!open ? <IoMenuOutline /> : <IoCloseOutline />}
 						</div>
 					</div>
 					<div

3d391340fa87580e9919407ae02c89c06a3fd4a6
Kelechukwu <paparazzip8@gmail.com>
2023-07-23 23:20:11 +0100

    created page, will implement functionalities soon

---
+++ src/app/thanks/page.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<>
+			<div className="m-10 pb-10">
+				<div className="text-3xl px-4 py-8 bg-gray-50">
+					<div className="border-b-8 borer-w">Thank you!</div>
+				</div>
+				<div className="mt-10 text-lg">
+					<div>What happens next?</div>
+					<div>
+						We will inform suitable professionals about your and interested
+						professionals will call you.
+					</div>
+					<div className="mt-10">
+						Professionals take their time to call you. Please acknowledge and
+						respond to thier calls, they will appreciate it.
+					</div>
+					<div className="my-10">
+						Kindly let us know immediately you hire someone so that
+						professionals cans top contacting you.
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default page;

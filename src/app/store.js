c7bd4fa09c69ab8bcc552828b756e90237d977ca
Kelechukwu <paparazzip8@gmail.com>
2023-08-22 16:04:04 +0100

    removed local storage functionality

--- src/app/store.js
+++ src/app/store.js
@@ -9,30 +9,29 @@ import { createSlice } from "@reduxjs/toolkit";
 // Check if window is defined (i.e., running in the browser)
 const isBrowser = typeof window !== "undefined";
 
-let parsedLocalStorageLocation = [];
+let parsedLocalStorageUser = [];
 
 if (isBrowser) {
-	const localStorageLocation = localStorage.getItem("userInfo");
-	parsedLocalStorageLocation = localStorageLocation
-		? JSON.parse(localStorageLocation)
-		: [];
+	const localStorageUser = localStorage.getItem("userInfo");
+	parsedLocalStorageUser = localStorageUser ? JSON.parse(localStorageUser) : [];
 }
 const initialState = {
-	value: parsedLocalStorageLocation,
+	value: parsedLocalStorageUser,
 };
 
-export const userInfoSlice = createSlice({
-	name: "userInfo",
+export const userSlice = createSlice({
+	name: "user",
 	initialState,
 	reducers: {
-		addUserInfo: (state, action) => {
-			// state.value.push(action.payload);
-			// console.log(state.value);
+		setNewDocId: (state, action) => {
+			//set the state to action.payload
+			state.value = action.payload;
+			console.log(state.value);
 			// Create a new array with the updated value
-			const updatedValue = [...state.value, action.payload];
-			state.value = updatedValue; // Update the state
+			// const updatedValue = [...state.value, action.payload];
+			// state.value = updatedValue; // Update the state
 			localStorage.setItem("userInfo", JSON.stringify(state.value));
 		},
 	},
 });
-export const { addUserInfo } = userInfoSlice.actions;
+export const { setNewDocId } = userSlice.actions;

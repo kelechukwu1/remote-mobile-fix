be0b21b4163fe0ede88ca4cb38ee3d18a8ba8b52
Kelechukwu <paparazzip8@gmail.com>
2023-08-26 19:56:51 +0100

    .

--- src/app/question/page.jsx
+++ src/app/question/page.jsx
@@ -1,130 +1,131 @@
-// "use client";
+"use client";
 
-// import { BsArrowLeft } from "react-icons/bs";
-// import { useRouter } from "next/navigation";
-// import { useEffect, useState } from "react";
-// import Link from "next/link";
-// import { db } from "../config/firebase";
-// import { updateDoc, doc } from "firebase/firestore";
-// import { useSelector } from "react-redux";
+import { BsArrowLeft } from "react-icons/bs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { db } from "../config/firebase";
+import { updateDoc, doc } from "firebase/firestore";
+import { useSelector } from "react-redux";
 
-// const page = () => {
-// 	//init nextJs router navigation
-// 	const router = useRouter();
+const page = () => {
+	//init nextJs router navigation
+	const router = useRouter();
 
-// 	//get rtk value
-// 	const userId = useSelector((state) => state.user.value);
-// 	console.log(userId);
+	// //get rtk value
+	// const userId = useSelector((state) => state.user.value);
+	// console.log(userId);
 
-// 	//firestore repairers ref
-// 	const initialUserRef = doc(db, "user", userId);
-// 	// create state for the input fields
-// 	const [email, setEmail] = useState("");
-// 	const [phone, setPhone] = useState("");
-// 	// create state for the input field errors
-// 	const [emailErr, setEmailErr] = useState("");
-// 	const [phoneErr, setPhoneErr] = useState("");
-// 	// const handleRef = useRef();
-// 	// console.log(handleRef);
+	// //firestore repairers ref
+	// const initialUserRef = doc(db, "user", userId);
+	// create state for the input fields
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	// create state for the input field errors
+	const [emailErr, setEmailErr] = useState("");
+	const [phoneErr, setPhoneErr] = useState("");
+	// const handleRef = useRef();
+	// console.log(handleRef);
 
-// 	//clear input error function
-// 	useEffect(() => {
-// 		let timeoutId;
+	//clear input error function
+	useEffect(() => {
+		let timeoutId;
 
-// 		if (setEmailErr || setPhoneErr) {
-// 			timeoutId = setTimeout(() => {
-// 				setEmailErr("");
-// 				setPhoneErr("");
-// 			}, 3000);
-// 		}
+		if (setEmailErr || setPhoneErr) {
+			timeoutId = setTimeout(() => {
+				setEmailErr("");
+				setPhoneErr("");
+			}, 3000);
+		}
 
-// 		return () => {
-// 			clearTimeout(timeoutId);
-// 		};
-// 	}, [emailErr, phoneErr]);
-// 	//handleSubmit function
-// 	const handleSubmit = async (e) => {
-// 		e.preventDefault();
-// 		const emailRegEx =
-// 			/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-// 		if (!emailRegEx.test(email)) {
-// 			setEmailErr("Please enter a valid email");
-// 		} else if (phone === "") {
-// 			setPhoneErr("Field must not be empty");
-// 		} else {
-// 			router.push("/problemDescription");
-// 			//add data to firebase
-// 			await updateDoc(
-// 				initialUserRef,
-// 				{
-// 					email: email,
-// 					phone: phone,
-// 				},
-// 				{ merge: true }
-// 			);
-// 		}
-// 	};
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	}, [emailErr, phoneErr]);
+	//handleSubmit function
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const emailRegEx =
+			/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+		if (!emailRegEx.test(email)) {
+			setEmailErr("Please enter a valid email");
+		} else if (phone === "") {
+			setPhoneErr("Field must not be empty");
+		} else {
+			// router.push("/problemDescription");
+			// //add data to firebase
+			// await updateDoc(
+			// 	initialUserRef,
+			// 	{
+			// 		email: email,
+			// 		phone: phone,
+			// 	},
+			// 	{ merge: true }
+			// );
+			console.log("done");
+		}
+	};
 
-// 	return (
-// 		<div className="bg-gray-100 h-screen lg:bg-gray-50 py-5 md:px-36 lg:px-80 justify-center items-center">
-// 			<div
-// 				className="mx-6 mb-5 w-14 py-2 rounded-md cursor-pointer bg-slate-900 hover:bg-slate-950 transition duration-500 text-white"
-// 				onClick={() => router.push("/")}
-// 			>
-// 				<BsArrowLeft className="w-6 h-6 ml-4" />
-// 			</div>
-// 			<div className="mx-6 gap-4">
-// 				<form onSubmit={handleSubmit}>
-// 					<div className="text-2xl font-semibold mb-5 text-center">
-// 						We would like to contact you
-// 					</div>
+	return (
+		<div className="bg-gray-100 h-screen lg:bg-gray-50 py-5 md:px-36 lg:px-80 justify-center items-center">
+			<div
+				className="mx-6 mb-5 w-14 py-2 rounded-md cursor-pointer bg-slate-900 hover:bg-slate-950 transition duration-500 text-white"
+				onClick={() => router.push("/")}
+			>
+				<BsArrowLeft className="w-6 h-6 ml-4" />
+			</div>
+			<div className="mx-6 gap-4">
+				<form onSubmit={handleSubmit}>
+					<div className="text-2xl font-semibold mb-5 text-center">
+						We would like to contact you
+					</div>
 
-// 					<div className="text-xl mb-3">
-// 						<div>Enter your email</div>
-// 						<input
-// 							onChange={(e) => {
-// 								setEmail(e.target.value);
-// 							}}
-// 							type="text"
-// 							placeholder="test@test.com"
-// 							className="w-full p-3 rounded-lg text-xl"
-// 						/>
-// 						{emailErr && (
-// 							<div className="pt-2 text-red-500 text-sm">{emailErr}</div>
-// 						)}
-// 					</div>
-// 					<div className="text-xl mb-3">
-// 						<div>Enter your phone number</div>
-// 						<input
-// 							onChange={(e) => {
-// 								setPhone(e.target.value);
-// 							}}
-// 							type="text"
-// 							placeholder="+234.........."
-// 							className="w-full p-3 rounded-lg text-xl"
-// 						/>
-// 						{phoneErr && (
-// 							<div className="pt-2 text-red-500 text-sm">{phoneErr}</div>
-// 						)}
-// 					</div>
-// 					<div className="flex justify-between mt-4">
-// 						<div>
-// 							<Link href={"/"}>
-// 								<div className="bg-gray-300 hover:bg-gray-500 text-xl p-2 rounded transition duration-500">
-// 									Go Back
-// 								</div>
-// 							</Link>
-// 						</div>
-// 						<div>
-// 							<div className="bg-slate-800 hover:bg-slate-950 text-white text-xl p-2 rounded transition duration-500">
-// 								<button>Continue</button>
-// 							</div>
-// 						</div>
-// 					</div>
-// 				</form>
-// 			</div>
-// 		</div>
-// 	);
-// };
+					<div className="text-xl mb-3">
+						<div>Enter your email</div>
+						<input
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+							type="text"
+							placeholder="test@test.com"
+							className="w-full p-3 rounded-lg text-xl"
+						/>
+						{emailErr && (
+							<div className="pt-2 text-red-500 text-sm">{emailErr}</div>
+						)}
+					</div>
+					<div className="text-xl mb-3">
+						<div>Enter your phone number</div>
+						<input
+							onChange={(e) => {
+								setPhone(e.target.value);
+							}}
+							type="text"
+							placeholder="+234.........."
+							className="w-full p-3 rounded-lg text-xl"
+						/>
+						{phoneErr && (
+							<div className="pt-2 text-red-500 text-sm">{phoneErr}</div>
+						)}
+					</div>
+					<div className="flex justify-between mt-4">
+						<div>
+							<Link href={"/"}>
+								<div className="bg-gray-300 hover:bg-gray-500 text-xl p-2 rounded transition duration-500">
+									Go Back
+								</div>
+							</Link>
+						</div>
+						<div>
+							<div className="bg-slate-800 hover:bg-slate-950 text-white text-xl p-2 rounded transition duration-500">
+								<button>Continue</button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
 
-// export default page;
+export default page;
--- src/app/signUp/page.jsx
+++ src/app/signUp/page.jsx
@@ -1,322 +1,323 @@
-// "use client";
+"use client";
 
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/navigation";
-// import { db } from "../config/firebase";
-// import { updateDoc, doc } from "firebase/firestore";
-// import { auth } from "../config/firebase";
-// import { useSelector } from "react-redux";
-// import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { db } from "../config/firebase";
+import { updateDoc, doc } from "firebase/firestore";
+import { auth } from "../config/firebase";
+import { useSelector } from "react-redux";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-// const page = () => {
-// 	//init next router
-// 	const router = useRouter();
-// 	//get rtk value
-// 	const userId = useSelector((state) => state.user.value);
+const page = () => {
+	//init next router
+	const router = useRouter();
+	// //get rtk value
+	// const userId = useSelector((state) => state.user.value);
 
-// 	//firestore repairers ref
-// 	const initialRepairersRef = doc(db, "repairers", userId);
+	// //firestore repairers ref
+	// const initialRepairersRef = doc(db, "repairers", userId);
 
-// 	//FUNCTIONS BELOW TAKES CARE OF INPUT VALIDATIONS AND SUBMIT EVENTS
-// 	//get form values
-// 	const [values, setValues] = useState({
-// 		email: "",
-// 		confirmEmail: "",
-// 		firstName: "",
-// 		lastName: "",
-// 		userName: "",
-// 		phone: "",
-// 		confirmPhone: "",
-// 		password: "",
-// 	});
-// 	//seperate checkbox state
-// 	const [checked, setChecked] = useState(false);
+	//FUNCTIONS BELOW TAKES CARE OF INPUT VALIDATIONS AND SUBMIT EVENTS
+	//get form values
+	const [values, setValues] = useState({
+		email: "",
+		confirmEmail: "",
+		firstName: "",
+		lastName: "",
+		userName: "",
+		phone: "",
+		confirmPhone: "",
+		password: "",
+	});
+	//seperate checkbox state
+	const [checked, setChecked] = useState(false);
 
-// 	//handleChange function
-// 	const handleChange = (e) => {
-// 		setValues({ ...values, [e.target.name]: e.target.value });
-// 	};
+	//handleChange function
+	const handleChange = (e) => {
+		setValues({ ...values, [e.target.name]: e.target.value });
+	};
 
-// 	//error state
-// 	// let error = {};
-// 	const [emailErr, setEmailErr] = useState("");
-// 	const [confirmEmailErr, setConfirmEmailErr] = useState("");
-// 	const [firstNameErr, setFirstNameErr] = useState("");
-// 	const [lastNameErr, setLastNameErr] = useState("");
-// 	const [userNameErr, setUserNameErr] = useState("");
-// 	const [phoneErr, setPhoneErr] = useState("");
-// 	const [confirmPhoneErr, setConfirmPhoneErr] = useState("");
-// 	const [PasswordErr, setPasswordErr] = useState("");
-// 	const [isCheckedErr, setIsCheckedErr] = useState("");
+	//error state
+	// let error = {};
+	const [emailErr, setEmailErr] = useState("");
+	const [confirmEmailErr, setConfirmEmailErr] = useState("");
+	const [firstNameErr, setFirstNameErr] = useState("");
+	const [lastNameErr, setLastNameErr] = useState("");
+	const [userNameErr, setUserNameErr] = useState("");
+	const [phoneErr, setPhoneErr] = useState("");
+	const [confirmPhoneErr, setConfirmPhoneErr] = useState("");
+	const [PasswordErr, setPasswordErr] = useState("");
+	const [isCheckedErr, setIsCheckedErr] = useState("");
 
-// 	//clear error function
-// 	useEffect(() => {
-// 		let timeoutId;
+	//clear error function
+	useEffect(() => {
+		let timeoutId;
 
-// 		if (
-// 			emailErr ||
-// 			confirmEmailErr ||
-// 			firstNameErr ||
-// 			lastNameErr ||
-// 			userNameErr ||
-// 			phoneErr ||
-// 			confirmPhoneErr ||
-// 			PasswordErr ||
-// 			isCheckedErr
-// 		) {
-// 			timeoutId = setTimeout(() => {
-// 				setEmailErr("");
-// 				setConfirmEmailErr("");
-// 				setFirstNameErr("");
-// 				setLastNameErr("");
-// 				setUserNameErr("");
-// 				setPhoneErr("");
-// 				setConfirmPhoneErr("");
-// 				setPasswordErr("");
-// 				setIsCheckedErr("");
-// 			}, 3000);
-// 		}
+		if (
+			emailErr ||
+			confirmEmailErr ||
+			firstNameErr ||
+			lastNameErr ||
+			userNameErr ||
+			phoneErr ||
+			confirmPhoneErr ||
+			PasswordErr ||
+			isCheckedErr
+		) {
+			timeoutId = setTimeout(() => {
+				setEmailErr("");
+				setConfirmEmailErr("");
+				setFirstNameErr("");
+				setLastNameErr("");
+				setUserNameErr("");
+				setPhoneErr("");
+				setConfirmPhoneErr("");
+				setPasswordErr("");
+				setIsCheckedErr("");
+			}, 3000);
+		}
 
-// 		return () => {
-// 			clearTimeout(timeoutId);
-// 		};
-// 	}, [
-// 		emailErr,
-// 		confirmEmailErr,
-// 		firstNameErr,
-// 		lastNameErr,
-// 		userNameErr,
-// 		phoneErr,
-// 		confirmPhoneErr,
-// 		PasswordErr,
-// 		isCheckedErr,
-// 	]);
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	}, [
+		emailErr,
+		confirmEmailErr,
+		firstNameErr,
+		lastNameErr,
+		userNameErr,
+		phoneErr,
+		confirmPhoneErr,
+		PasswordErr,
+		isCheckedErr,
+	]);
 
-// 	const handleSubmit = async (e) => {
-// 		e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-// 		//email regex
-// 		const emailRegEx =
-// 			/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-// 		// //name input regEx
-// 		// const nameRe = /^[a-zA-Z]{2,15}$/;
-// 		// //address input regEx
-// 		// const addressRe = /^[a-zA-Z]{2,30}\w$/;
-// 		// //city input regEx
-// 		// const cityRe = /^[a-zA-Z]{2,10}\w$/;
+		//email regex
+		const emailRegEx =
+			/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+		// //name input regEx
+		// const nameRe = /^[a-zA-Z]{2,15}$/;
+		// //address input regEx
+		// const addressRe = /^[a-zA-Z]{2,30}\w$/;
+		// //city input regEx
+		// const cityRe = /^[a-zA-Z]{2,10}\w$/;
 
-// 		if (!emailRegEx.test(values.email)) {
-// 			setEmailErr("Input a valid email");
-// 		} else if (values.email != values.confirmEmail) {
-// 			setConfirmEmailErr("Email must match");
-// 		} else if (values.firstName === "") {
-// 			setFirstNameErr("Invalid field");
-// 		} else if (values.lastName === "") {
-// 			setLastNameErr("Invalid field");
-// 		} else if (values.userName === "") {
-// 			setUserNameErr("Username must not be empty");
-// 		} else if (values.userName.length < 5) {
-// 			setUserNameErr("Username must not be less than 5 char");
-// 		} else if (values.phone === "") {
-// 			setPhoneErr("Invalid field");
-// 		} else if (values.confirmPhone === "") {
-// 			setConfirmPhoneErr("Invalid field");
-// 		} else if (values.confirmPhone != values.phone) {
-// 			setConfirmPhoneErr("Phone must match");
-// 		} else if (values.password === "") {
-// 			setPasswordErr("Password must not be empty");
-// 		} else if (values.password < 6) {
-// 			setPasswordErr("Password must be up to 6 characters");
-// 		} else if (values.isChecked === false) {
-// 			setIsCheckedErr("You have not accepted our terms of service");
-// 		} else {
-// 			//redirect the page
-// 			checked && router.push("/dashboard");
-// 			//merge data to firebase id=== rtk id
-// 			await updateDoc(
-// 				initialRepairersRef,
-// 				{
-// 					email: values.email,
-// 					firstName: values.firstName,
-// 					lastName: values.lastName,
-// 					userName: values.userName,
-// 					phone: values.phone,
-// 					password: values.password,
-// 				},
-// 				{ merge: true }
-// 			);
-// 			//implement firebase auth sign up
-// 			try {
-// 				await createUserWithEmailAndPassword(
-// 					auth,
-// 					values.email,
-// 					values.password
-// 				);
-// 			} catch (err) {
-// 				console.error(err.message);
-// 			}
-// 		}
-// 	};
-// 	return (
-// 		<>
-// 			<div className="py-5 bg-gray-100 lg:bg-gray-50 md:px-36 lg:px-80">
-// 				<div className="items-center px-5 py-10 mx-5 bg-white shadow-lg rounded-2xl">
-// 					<div className="text-3xl">The easiest way to find new customers</div>
-// 					<form onSubmit={handleSubmit}>
-// 						<div className="my-5 text-xl text-center">Setup your account</div>
-// 						<div className="mb-5 ">
-// 							<label className="text-md">Email address</label>
-// 							<input
-// 								onChange={handleChange}
-// 								name="email"
-// 								value={values.email}
-// 								type="text"
-// 								className="w-full p-2 border border-gray-500 rounded-lg text-md"
-// 								placeholder="Enter your email address"
-// 							/>
-// 							{emailErr && (
-// 								<div className="text-sm text-red-500">{emailErr}</div>
-// 							)}
-// 						</div>
-// 						<div className="mb-5 ">
-// 							<label className="text-md">Confirm email address</label>
-// 							<input
-// 								onChange={handleChange}
-// 								name="confirmEmail"
-// 								value={values.confirmEmail}
-// 								type="text"
-// 								className="w-full p-2 border border-gray-500 rounded-lg text-md"
-// 								placeholder="Enter your email address"
-// 							/>
-// 							{confirmEmailErr && (
-// 								<div className="text-sm text-red-500">{confirmEmailErr}</div>
-// 							)}
-// 						</div>
+		if (!emailRegEx.test(values.email)) {
+			setEmailErr("Input a valid email");
+		} else if (values.email != values.confirmEmail) {
+			setConfirmEmailErr("Email must match");
+		} else if (values.firstName === "") {
+			setFirstNameErr("Invalid field");
+		} else if (values.lastName === "") {
+			setLastNameErr("Invalid field");
+		} else if (values.userName === "") {
+			setUserNameErr("Username must not be empty");
+		} else if (values.userName.length < 5) {
+			setUserNameErr("Username must not be less than 5 char");
+		} else if (values.phone === "") {
+			setPhoneErr("Invalid field");
+		} else if (values.confirmPhone === "") {
+			setConfirmPhoneErr("Invalid field");
+		} else if (values.confirmPhone != values.phone) {
+			setConfirmPhoneErr("Phone must match");
+		} else if (values.password === "") {
+			setPasswordErr("Password must not be empty");
+		} else if (values.password < 6) {
+			setPasswordErr("Password must be up to 6 characters");
+		} else if (values.isChecked === false) {
+			setIsCheckedErr("You have not accepted our terms of service");
+		} else {
+			// //redirect the page
+			// checked && router.push("/dashboard");
+			// //merge data to firebase id=== rtk id
+			// await updateDoc(
+			// 	initialRepairersRef,
+			// 	{
+			// 		email: values.email,
+			// 		firstName: values.firstName,
+			// 		lastName: values.lastName,
+			// 		userName: values.userName,
+			// 		phone: values.phone,
+			// 		password: values.password,
+			// 	},
+			// 	{ merge: true }
+			// );
+			// //implement firebase auth sign up
+			// try {
+			// 	await createUserWithEmailAndPassword(
+			// 		auth,
+			// 		values.email,
+			// 		values.password
+			// 	);
+			// } catch (err) {
+			// 	console.error(err.message);
+			// }
+			console.log("done");
+		}
+	};
+	return (
+		<>
+			<div className="py-5 bg-gray-100 lg:bg-gray-50 md:px-36 lg:px-80">
+				<div className="items-center px-5 py-10 mx-5 bg-white shadow-lg rounded-2xl">
+					<div className="text-3xl">The easiest way to find new customers</div>
+					<form onSubmit={handleSubmit}>
+						<div className="my-5 text-xl text-center">Setup your account</div>
+						<div className="mb-5 ">
+							<label className="text-md">Email address</label>
+							<input
+								onChange={handleChange}
+								name="email"
+								value={values.email}
+								type="text"
+								className="w-full p-2 border border-gray-500 rounded-lg text-md"
+								placeholder="Enter your email address"
+							/>
+							{emailErr && (
+								<div className="text-sm text-red-500">{emailErr}</div>
+							)}
+						</div>
+						<div className="mb-5 ">
+							<label className="text-md">Confirm email address</label>
+							<input
+								onChange={handleChange}
+								name="confirmEmail"
+								value={values.confirmEmail}
+								type="text"
+								className="w-full p-2 border border-gray-500 rounded-lg text-md"
+								placeholder="Enter your email address"
+							/>
+							{confirmEmailErr && (
+								<div className="text-sm text-red-500">{confirmEmailErr}</div>
+							)}
+						</div>
 
-// 						<div className="mb-5 ">
-// 							<label className="text-md">Name</label>
-// 							<div className="flex">
-// 								<div className="mr-1">
-// 									<input
-// 										onChange={handleChange}
-// 										name="firstName"
-// 										value={values.firstName}
-// 										type="text"
-// 										className="w-full p-2 border border-gray-500 rounded-lg text-md"
-// 										placeholder="Firstname"
-// 									/>
-// 									{firstNameErr && (
-// 										<div className="text-sm text-red-500">{firstNameErr}</div>
-// 									)}
-// 								</div>
-// 								<div className="ml-1">
-// 									<input
-// 										onChange={handleChange}
-// 										name="lastName"
-// 										value={values.lastName}
-// 										type="text"
-// 										className="w-full p-2 border border-gray-500 rounded-lg text-md"
-// 										placeholder="Lastname"
-// 									/>
-// 									{lastNameErr && (
-// 										<div className="text-sm text-red-500">{lastNameErr}</div>
-// 									)}
-// 								</div>
-// 							</div>
-// 						</div>
+						<div className="mb-5 ">
+							<label className="text-md">Name</label>
+							<div className="flex">
+								<div className="mr-1">
+									<input
+										onChange={handleChange}
+										name="firstName"
+										value={values.firstName}
+										type="text"
+										className="w-full p-2 border border-gray-500 rounded-lg text-md"
+										placeholder="Firstname"
+									/>
+									{firstNameErr && (
+										<div className="text-sm text-red-500">{firstNameErr}</div>
+									)}
+								</div>
+								<div className="ml-1">
+									<input
+										onChange={handleChange}
+										name="lastName"
+										value={values.lastName}
+										type="text"
+										className="w-full p-2 border border-gray-500 rounded-lg text-md"
+										placeholder="Lastname"
+									/>
+									{lastNameErr && (
+										<div className="text-sm text-red-500">{lastNameErr}</div>
+									)}
+								</div>
+							</div>
+						</div>
 
-// 						<div className="mb-5 ">
-// 							<label className="text-md">Username</label>
-// 							<input
-// 								onChange={handleChange}
-// 								name="userName"
-// 								value={values.userName}
-// 								type="text"
-// 								className="w-full p-2 border border-gray-500 rounded-lg text-md"
-// 								placeholder=""
-// 							/>
-// 							{userNameErr && (
-// 								<div className="text-sm text-red-500">{userNameErr}</div>
-// 							)}
-// 						</div>
+						<div className="mb-5 ">
+							<label className="text-md">Username</label>
+							<input
+								onChange={handleChange}
+								name="userName"
+								value={values.userName}
+								type="text"
+								className="w-full p-2 border border-gray-500 rounded-lg text-md"
+								placeholder=""
+							/>
+							{userNameErr && (
+								<div className="text-sm text-red-500">{userNameErr}</div>
+							)}
+						</div>
 
-// 						<div className="mb-5 ">
-// 							<label className="text-md">Phone</label>
-// 							<div className="flex">
-// 								<div className="mr-1 w-[1/2]">
-// 									<input
-// 										onChange={handleChange}
-// 										name="phone"
-// 										value={values.phone}
-// 										type="text"
-// 										className="w-full p-2 border border-gray-500 rounded-lg text-md"
-// 										placeholder="phone"
-// 									/>
-// 									{phoneErr && (
-// 										<div className="text-sm text-red-500">{phoneErr}</div>
-// 									)}
-// 								</div>
-// 								<div className="ml-1 w-[1/2]">
-// 									<input
-// 										onChange={handleChange}
-// 										name="confirmPhone"
-// 										value={values.confirmPhone}
-// 										type="text"
-// 										className="w-full p-2 border border-gray-500 rounded-lg text-md"
-// 										placeholder="confirm phone"
-// 									/>
-// 									{confirmPhoneErr && (
-// 										<div className="text-sm text-red-500">
-// 											{confirmPhoneErr}
-// 										</div>
-// 									)}
-// 								</div>
-// 							</div>
-// 						</div>
-// 						<div className="mb-5 ">
-// 							<label className="text-md">Password</label>
-// 							<input
-// 								onChange={handleChange}
-// 								name="password"
-// 								value={values.password}
-// 								type="password"
-// 								className="w-full p-2 border border-gray-500 rounded-lg text-md"
-// 								placeholder="Password"
-// 							/>
-// 							{PasswordErr && (
-// 								<div className="text-sm text-red-500">{PasswordErr}</div>
-// 							)}
-// 						</div>
+						<div className="mb-5 ">
+							<label className="text-md">Phone</label>
+							<div className="flex">
+								<div className="mr-1 w-[1/2]">
+									<input
+										onChange={handleChange}
+										name="phone"
+										value={values.phone}
+										type="text"
+										className="w-full p-2 border border-gray-500 rounded-lg text-md"
+										placeholder="phone"
+									/>
+									{phoneErr && (
+										<div className="text-sm text-red-500">{phoneErr}</div>
+									)}
+								</div>
+								<div className="ml-1 w-[1/2]">
+									<input
+										onChange={handleChange}
+										name="confirmPhone"
+										value={values.confirmPhone}
+										type="text"
+										className="w-full p-2 border border-gray-500 rounded-lg text-md"
+										placeholder="confirm phone"
+									/>
+									{confirmPhoneErr && (
+										<div className="text-sm text-red-500">
+											{confirmPhoneErr}
+										</div>
+									)}
+								</div>
+							</div>
+						</div>
+						<div className="mb-5 ">
+							<label className="text-md">Password</label>
+							<input
+								onChange={handleChange}
+								name="password"
+								value={values.password}
+								type="password"
+								className="w-full p-2 border border-gray-500 rounded-lg text-md"
+								placeholder="Password"
+							/>
+							{PasswordErr && (
+								<div className="text-sm text-red-500">{PasswordErr}</div>
+							)}
+						</div>
 
-// 						<div className="flex items-center ">
-// 							<input
-// 								onChange={(e) => {
-// 									setChecked(e.target.checked);
-// 								}}
-// 								type="checkbox"
-// 								name="checkbox"
-// 								className="w-6 h-6 mr-3 border-2 cursor-pointer rounded-3xl"
-// 							/>
-// 							<label className="text-md">
-// 								I agree to the{" "}
-// 								<span className="text-blue-500">Terms of service</span>
-// 							</label>
-// 						</div>
-// 						{isCheckedErr && (
-// 							<div className="text-sm text-red-500">{isCheckedErr}</div>
-// 						)}
+						<div className="flex items-center ">
+							<input
+								onChange={(e) => {
+									setChecked(e.target.checked);
+								}}
+								type="checkbox"
+								name="checkbox"
+								className="w-6 h-6 mr-3 border-2 cursor-pointer rounded-3xl"
+							/>
+							<label className="text-md">
+								I agree to the{" "}
+								<span className="text-blue-500">Terms of service</span>
+							</label>
+						</div>
+						{isCheckedErr && (
+							<div className="text-sm text-red-500">{isCheckedErr}</div>
+						)}
 
-// 						<button
-// 							className="w-full p-3 mt-5 text-white transition duration-500 rounded-lg text-md bg-slate-900 hover:bg-slate-950"
-// 							type="submit"
-// 						>
-// 							Register
-// 						</button>
-// 					</form>
-// 				</div>
-// 			</div>
-// 		</>
-// 	);
-// };
+						<button
+							className="w-full p-3 mt-5 text-white transition duration-500 rounded-lg text-md bg-slate-900 hover:bg-slate-950"
+							type="submit"
+						>
+							Register
+						</button>
+					</form>
+				</div>
+			</div>
+		</>
+	);
+};
 
-// export default page;
+export default page;
